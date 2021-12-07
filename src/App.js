a170ff734b661cd5d82dcc5fccae95a067874a15
rickyalmeida <ricky.almeida@rxpro.com.br>
2021-12-07 18:43:23 -0300

    create form to enter phone number and open chat

--- src/App.js
+++ src/App.js
@@ -1,3 +1,33 @@
-const App = () => <h1>WhatsApp Chat Opener</h1>
+import {useState} from 'react'
+import {mask, unmask} from './utils/phone'
+
+const App = () => {
+  const [phone, setPhone] = useState('')
+
+  const handleChange = event => {
+    setPhone(mask(event.target.value))
+  }
+
+  const handleSubmit = event => {
+    event.preventDefault()
+    const url = `https://api.whatsapp.com/send?phone=${unmask(phone)}`
+    window.open(url, '_blank')
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="phone-input">Número: </label>
+      <input
+        type="tel"
+        name="phone"
+        id="phone-input"
+        placeholder="(xx) xxxxx-xxxx"
+        onChange={handleChange}
+        value={phone}
+      />
+      <button type="submit">Ir</button>
+    </form>
+  )
+}
 
 export default App
