e64edec3c4105958954bbfe68f025d92368f6aab
rickyalmeida <ricky.almeida@rxpro.com.br>
2021-12-07 19:54:21 -0300

    remove measurement id from firebase config

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -8,7 +8,6 @@ const options = {
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_APP_ID,
-  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 }
 
 export const app = initializeApp(options)
