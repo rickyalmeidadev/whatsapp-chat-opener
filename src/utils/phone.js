2adbcf474299106ac9f16c8d503d552c67099819
rickyalmeida <ricky.almeida@rxpro.com.br>
2021-12-07 18:43:04 -0300

    create phone mask and unmask utils

---
+++ src/utils/phone.js
@@ -0,0 +1,9 @@
+export const mask = value =>
+  value
+    .replace(/\D/g, '')
+    .replace(/(\d{2})(\d)/, '($1) $2')
+    .replace(/(\d{4})(\d)/, '$1-$2')
+    .replace(/(\d{4})-(\d)(\d{4})/, '$1$2-$3')
+    .replace(/(-\d{4})\d+?$/, '$1')
+
+export const unmask = value => value.replace(/\D/g, '')
